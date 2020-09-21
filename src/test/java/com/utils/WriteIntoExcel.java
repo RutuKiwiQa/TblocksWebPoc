256ecac16b4279f20bd7b0dc483d7d71a8c6bfa3
Rutu <rutu.shah@kiwiqa.com>
2020-09-21 09:34:56 +0530

    September 21rst 2020

--- src/test/java/com/utils/WriteIntoExcel.java
+++ src/test/java/com/utils/WriteIntoExcel.java
@@ -76,58 +76,5 @@ public class WriteIntoExcel {
 
     }
 
-    public static void setDataExcel(String filename, String sheetname,  ArrayList<String> mainUrl, ArrayList<String> subUrl, ArrayList<Integer> expectedResult, ArrayList<Integer> actualResult,ArrayList<String> executedStatus) throws IOException {
-        File datafile = new File(filename);
-        String fullpath = datafile.getAbsolutePath();
-        ExcelWBook = new XSSFWorkbook(fullpath);
-        ExcelWSheet = ExcelWBook.getSheet(sheetname);
-        int totalRows = ExcelWSheet.getLastRowNum();
-        try {
-            int rowno = totalRows + 1;
-            FileInputStream inputStream = new FileInputStream(new File(fullpath));
-            @SuppressWarnings("resource")
-            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
-            XSSFSheet firstSheet = workbook.getSheetAt(0);
-//			XSSFRow row = firstSheet.createRow(rowno);
-
-            CellStyle style1 = workbook.createCellStyle();
-           // style1.setFillForegroundColor(IndexedColors.BLACK.getIndex());
-           // style1.setFillPattern(CellStyle.SOLID_FOREGROUND);
-           // Font font = workbook.createFont();
-           // font.setColor(IndexedColors.AQUA.getIndex());
-//	        style1.setFillBackgroundColor(IndexedColors.BLACK.getIndex());
-          //  font.setBold(true);
-          //  style1.setFont(font);
-
-
-            for(int i=0;i<mainUrl.size() && i<subUrl.size() && i<expectedResult.size() && i<actualResult.size() && i<executedStatus.size();i++)
-            {
-                Row rw= firstSheet.createRow(i);
-                Cell cl1=rw.createCell(0);
-                cl1.setCellValue(mainUrl.get(i));
-               // cl1.setCellStyle(style1);
-
-                Cell cl2=rw.createCell(1);
-                cl2.setCellValue(subUrl.get(i));
-               // cl2.setCellStyle(style1);
-
-                Cell cl3 = rw.createCell(2);
-                cl3.setCellValue(expectedResult.get(i));
-
-                Cell cl4 = rw.createCell(3);
-                cl4.setCellValue(actualResult.get(i));
-
-                Cell cl5 = rw.createCell(4);
-                cl5.setCellValue(executedStatus.get(i));
-
-            }
-            inputStream.close();
-            FileOutputStream fos = new FileOutputStream(new File(fullpath));
-            workbook.write(fos);
-            fos.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
 }
 
